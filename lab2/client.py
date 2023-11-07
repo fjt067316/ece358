b02f8fb79d90f57250db59108865eb31b1eb12ef
Josh <joshuatfolkins@gmail.com>
2023-11-07 17:22:34 -0500

    done

---
+++ lab2/client.py
@@ -0,0 +1,107 @@
+from socket import *
+import random
+
+host = "127.0.0.1"
+port = 42069
+
+client_socket = socket(AF_INET, SOCK_DGRAM)
+
+def get_dns_header():
+    # Define the DNS header fields
+    random_id = random.getrandbits(16)
+    id = random_id  # A randomly generated 16-bit identifier
+    qr = 0  # qr = 0 for query, 1 for response
+    opcode = 0x0000  # opcode = 0 (standard query)
+    aa = 1  # aa = 0 (not authoritative)
+    tc = 0  # tc = 0 (not truncated)
+    rd = 1  # rd = 1 (recursion desired)
+    ra = 0  # ra = 0 (no recursion available)
+    z = 0  # z = 0
+    rcode = 0  # rcode = 0 (no error)
+    qdcount = 1  # qdcount = 1 (one entry in the question section)
+    ancount = 0  # ancount = 0 (no answers)
+    nscount = 0  # nscount = 0 (no name server records)
+    arcount = 0  # arcount = 0 (no additional records)
+
+    # Create a binary representation of the DNS header
+    header = (
+        (id).to_bytes(2, byteorder='big') +
+        ((qr << 15) | (opcode << 11) | (aa << 10) | (tc << 9) | (rd << 8) | (ra << 7) | (z << 4) | (rcode)).to_bytes(2, byteorder='big') +
+        (qdcount).to_bytes(2, byteorder='big') +
+        (ancount).to_bytes(2, byteorder='big') +
+        (nscount).to_bytes(2, byteorder='big') +
+        (arcount).to_bytes(2, byteorder='big')
+    )
+    
+    return bytes(header)
+
+def get_question_header(query):
+    # Define the values for the DNS question fields
+    lst = query.split(".")
+    qname = b""  # QNAME as bytes
+    
+    for word in lst:
+        word_length = len(word) & 0xFF  # Ensure it's an 8-bit value
+        qname += bytes([word_length])  # Append the 8-bit length
+        qname += word.encode('ascii')  # Append the ASCII values for the word
+
+    qtype = 0x0001  # qtype = 1 for type A (host address query)
+    qclass = 0x0001  # qclass for IN (Internet)  
+
+    # Create a binary representation
+    question = qname + b"\x00" + qtype.to_bytes(2, byteorder='big') + qclass.to_bytes(2, byteorder='big')
+
+    return question
+
+def decode_answer(dns_resp, question_offset):
+    if len(dns_resp) >= 12:
+        header = dns_resp[:12]
+        ancount = int.from_bytes(header[6:8], byteorder='big')
+        
+        offset = question_offset+12  # Initial offset to the start of answer sections
+
+        for i in range(ancount):
+            answer_section = dns_resp[offset:offset + 12]
+
+            name = int.from_bytes(answer_section[0:2], byteorder='big')
+            rtype = int.from_bytes(answer_section[2:4], byteorder='big')
+            rclass = ''.join(chr(byte) for byte in answer_section[4:6])
+            ttl = int.from_bytes(answer_section[6:10], byteorder='big')
+            rdlength = int.from_bytes(answer_section[10:12], byteorder='big')
+            rdata_bytes = dns_resp[offset + 12:offset + 12 + rdlength]
+
+            # Convert the bytes to an IP address string
+            rdata = '.'.join(str(byte) for byte in rdata_bytes)
+            
+            
+            print(f"google.com {rtype} {rclass} {ttl} {rdata}")
+            
+            offset += 12 + rdlength
+    
+        
+while True:
+    query = input("Enter Domain Name: ")
+    
+    if query == "end":
+        client_socket.sendto(b'end', (host, port))
+        break
+    
+    question_header = get_question_header(query)
+    
+    # hex_q_header = ' '.join(format(byte, '02x') for byte in question_header)
+    # print(hex_q_header)
+
+    dns_header = get_dns_header()
+    dns_query = dns_header + question_header
+    client_socket.sendto(dns_query, (host, port))
+    
+    data, server_address = client_socket.recvfrom(2048)
+    
+    hex_data = ' '.join(format(byte, '02x') for byte in data)
+    print(hex_data)
+
+    # hex_data = ''.join(format(byte, '02x') for byte in data)
+    decode_answer(data, len(question_header))
+    # print(data)
+
+client_socket.close()
---
+++ lab2/server.py
@@ -0,0 +1,133 @@
+from socket import *
+from datetime import datetime
+import random
+
+host = "127.0.0.1"
+port = 42069
+
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+serverSocket.bind((host, port))
+
+# key is "google.com" value is (google.com, A, IN, 260, 192.165.1.1)
+domain_map = {
+    "google.com": [
+        ("google.com", 1, "IN", 260, "192.165.1.1"),
+        ("google.com", 1, "IN", 260, "192.165.1.10")
+    ],
+    "youtube.com" : [("youtube.com", 1, "IN", 160, "192.165.1.2")],
+    "uwaterloo.ca" : [("uwaterloo.ca", 1, "IN", 160, "192.165.1.3")],
+    "wikipedia.org" : [("wikipedia.org", 1, "IN", 160, "192.165.1.4")],
+    "amazon.ca" : [("amazon.ca", 1, "IN", 160, "192.165.1.5")],
+}
+
+def get_dns_header(query):
+    random_id = random.getrandbits(16)
+
+    id = random_id  # A randomly generated 16-bit identifier
+    qr = 1  # qr = 1 for response 0 for query
+    opcode = 0  # opcode = 0 (standard query)
+    aa = 1  # aa = 1 (bit means valid response)
+    tc = 0  # tc = 0 (was response message truncated?)
+    rd = 0  # rd = 0 (no recursion requested from client)
+    ra = 0  # ra = 0 (no recursion available)
+    z = 0  # z = 0
+    rcode = 0  # rcode = 0 (no error)
+    qdcount = 1  # qdcount = 1 (one entry in the question section)
+    ancount = len(domain_map[query])  # ancount = number of resource records in the answer section
+    nscount = 0  # nscount = 0
+    arcount = 0  # arcount = 0
+
+    # Create a binary representation of the DNS response header
+    header = (
+        (id).to_bytes(2, byteorder='big') +
+        ((qr << 15) | (opcode << 11) | (aa << 10) | (tc << 9) | (rd << 8) | (ra << 7) | (z << 4) | (rcode)).to_bytes(2, byteorder='big') +
+        (qdcount).to_bytes(2, byteorder='big') +
+        (ancount).to_bytes(2, byteorder='big') +
+        (nscount).to_bytes(2, byteorder='big') +
+        (arcount).to_bytes(2, byteorder='big')
+    )
+    
+    return header
+
+
+def get_dns_query(recv_data):
+    
+    if len(recv_data) >= 12: # check if there is at least DNS header of 12 Bytes
+        dns_header = recv_data[:12]
+        question_section = recv_data[12:]
+        
+        # Parsing the QNAME field (domain name)
+        qname = b""
+        pointer = 0
+        while True:
+            label_length = question_section[pointer]
+            if label_length == 0:
+                # Zero-length label indicates the end of the domain name
+                break
+            label = question_section[pointer + 1 : pointer + 1 + label_length]
+            qname += label + b"."
+            pointer += label_length + 1
+
+        # Remove the trailing dot if it exists
+        if qname.endswith(b"."):
+            qname = qname[:-1]
+
+        return qname.decode('ascii')
+    
+
+def generate_dns_answer_section(domain):
+    # Create the binary representation of the DNS answer section
+    ans = b""
+    if domain in domain_map:
+        for record in domain_map[domain]:
+            # Get the values from the dictionary
+            values_list = record  # Assuming the first entry in the list
+
+            # Extract the relevant information
+            NAME = values_list[0]
+            TYPE = int(values_list[1])
+            CLASS = bytes(values_list[2].encode('ascii')) # Class IN
+            TTL = values_list[3]  # TTL value from the dictionary
+            RDATA = bytes(int(byte) for byte in values_list[4].split('.'))
+            RDLENGTH = len(RDATA)  # Length of RDATA (IPv4 address)
+
+            answer_section = (
+                # NAME (c00c in hex)
+                bytes([0xC0, 0x0C]) +
+                # TYPE (two octets containing TYPE A value, e.g., 0x0001 for A)
+                TYPE.to_bytes(2, byteorder='big') +
+                # CLASS (two octets containing IN value, e.g., 0x0001 for IN)
+                CLASS +
+                # TTL (32-bit unsigned integer)
+                TTL.to_bytes(4, byteorder='big') +
+                # RDLENGTH (16-bit unsigned integer)
+                RDLENGTH.to_bytes(2, byteorder='big') +
+                # RDATA (variable length string of octets)
+                RDATA 
+            )
+            
+            ans += answer_section
+
+    
+    return ans
+
+while True:
+    recv_data, addr_client = serverSocket.recvfrom(2048)
+
+    if(recv_data.decode('utf-8') == "end"):
+        break
+    
+    hex_data = ' '.join(format(byte, '02x') for byte in recv_data)
+    print(hex_data)
+    
+    qname = get_dns_query(recv_data)
+    print(qname)
+    header = get_dns_header(qname)
+    
+    
+    question_section = recv_data[12:]
+    
+    resp = header + question_section + generate_dns_answer_section(qname)
+    print(resp)
+    serverSocket.sendto(resp, addr_client)
+
